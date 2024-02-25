dcbdddcb771f15f3b9359aef6596b20d5ba8cdf2
tpeck428 <toripeck73@gmail.com>
2024-02-24 22:44:50 -0500

    part 1 updates

--- controlflow.js
+++ controlflow.js
@@ -1,5 +1,29 @@
-// Control Flow PArt 1
-const PI = 3.1415
-const area = PI * radius * radius
-const radius = 5
+// Control Flow Part 1
+// const PI = 3.1415
+// const radius = 5
+// const area = PI * radius * radius
 
+// Each plant requires a minimum space of 0.8 square meters.
+// The area is starting with 20 plants.
+// The plants double in number every week.
+
+//Predict plant grow after a specific number of weeks
+const plant = 20;
+const week0 = 1
+const week1 = 2;
+const week2 = 4;
+const week3 = 8;
+
+//Week One = 
+
+let = (growth = plant * week1); {
+    console.log(growth);
+}
+
+if (growth > 78) {
+    console.log('Prune');
+} else if (growth < 39) {
+    console.log('Plant More'); 
+} else {
+    console.log('Monitor')
+}
